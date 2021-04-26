1bd5f84ecaceec330e8b139c308e812a71eaf344
bakytzhan.manaspayev <bakytzhan.manaspayev@aitudata.kz>
2021-04-27 01:24:03 +0600

    init

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"keep-alive-grpc/pb"
+	"log"
+	"net"
+	// replace this with your own project
+)
+
+var counter int = 0
+
+func (s *server) SendRequest(cxt context.Context, r *pb.Request) (*pb.Response, error) {
+	counter++
+	return &pb.Response{Text: string(counter)}, nil
+}
+
+func (s *server) mustEmbedUnimplementedMyServiceServer() {
+	panic("implement me")
+}
+
+type server struct{}
+
+func main() {
+	lis, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatalf("Failed to listen:  %v", err)
+	}
+
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterMyServiceServer(grpcServer, &server{})
+	grpcServer.Serve(lis)
+}
